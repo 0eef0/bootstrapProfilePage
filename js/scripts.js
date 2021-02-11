67f8749ca293d1cbd7db6dad8f3cf50df7a3aa45
Ethan Roldan <erolda266@west-mec.org>
2021-02-10 18:19:38 -0700

    did some work

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 function about(){
     document.getElementById("mainDiv").innerHTML = "about page";
+    document.getElementById("about").innerHTML = "<strong>About Me</strong>";
+    document.getElementById("friends").innerHTML = "Friends";
+    document.getElementById("posts").innerHTML = "Posts";
 }
 function friends(){
     document.getElementById("mainDiv").innerHTML = "friends";
+    document.getElementById("friends").innerHTML = "<strong>Friends</strong>";
+    document.getElementById("about").innerHTML = "About Me";
+    document.getElementById("posts").innerHTML = "Posts";
 }
 function posts(){
     document.getElementById("mainDiv").innerHTML = "posts";
+    document.getElementById("posts").innerHTML = "<strong>Posts</strong>";
+    document.getElementById("about").innerHTML = "About Me";
+    document.getElementById("friends").innerHTML = "Friends";
 }
