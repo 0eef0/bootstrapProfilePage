2fa4d34e7a7e45ab06f5c81c12c61a67856dbe76
Ethan Roldan <erolda266@west-mec.org>
2021-02-10 17:58:10 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function about(){
+    document.getElementById("mainDiv").innerHTML = "about page";
+}
+function friends(){
+    document.getElementById("mainDiv").innerHTML = "friends";
+}
+function posts(){
+    document.getElementById("mainDiv").innerHTML = "posts";
+}
